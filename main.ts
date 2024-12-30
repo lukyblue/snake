b809875de274ab67da4d40ce6b0180db13575e6c
lukyblue <lukas@bendovi.cz>
2024-12-30 15:56:54 +0100

    Update main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,17 @@
 
 info.setScore(0)
+let had_pole: Sprite[] = []
+let jablicka = 3
 let strana = 0 // 1 - up, 2 - right, 3 - down, 4 - left
 let hlava_x = 8
 let hlava_y = 6
+let jablicko = sprites.create(assets.image`jablicko`, SpriteKind.Food)
 let had_hlava = sprites.create(assets.image`had_hlava`, SpriteKind.Player)
 had_hlava.z=100
+jablicko.z = 50
 scene.setBackgroundImage(assets.image`moon`)
 had_hlava.setPosition(pixel(hlava_x), pixel(hlava_y))
+jablicko.setPosition(pixel(10), pixel(10))
 //controller.moveSprite(had_hlava,100,100)
 
 let telo_kind = SpriteKind.create()
@@ -27,13 +32,20 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     strana = 4
 })
 
-game.onUpdateInterval(500, function() {
+game.onUpdateInterval(350, function() {
     if(strana==0){
         return
     }
     let had_telo = sprites.create(assets.image`had_telo`, telo_kind)
     had_telo.setPosition(pixel(hlava_x), pixel(hlava_y))
+    had_pole.push(had_telo)
+    if(had_pole.length>jablicka){
+        let zadek = had_pole.shift()
+        sprites.destroy(zadek)
+    }
+
     
+
     if (strana==1) {
         hlava_y--
         if (hlava_y<0){
@@ -69,6 +81,39 @@ game.onUpdateInterval(500, function() {
 sprites.onOverlap(SpriteKind.Player, telo_kind, function(sprite: Sprite, otherSprite: Sprite) {
     
     game.gameOver(false)
+})
+
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function(sprite: Sprite, otherSprite: Sprite) {
+    info.changeScoreBy(1)
+    jablicka++
+
+    music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
+
+    if (jablicka == 190) {
+        game.over(true)
+    }
+
+    while (true) {
+        jablicko.setPosition(pixel(randint(0, 15)), pixel(randint(0, 11)))
+        
+        if (jablicko.overlapsWith(had_hlava)) {
+            continue
+        }
+
+        let ok = true
+        for (let i = 0; i < had_pole.length; i++) {
+            if (jablicko.overlapsWith(had_pole[i])) {
+                ok = false
+                break
+            }
+        }
+
+        if (!ok) {
+            continue
+        }
+
+        break
+    }
 })
 
 
